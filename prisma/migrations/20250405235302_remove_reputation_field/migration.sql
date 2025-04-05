7a9a81ac16cc73454dda42b794856408f29f353b
Francisco Cedermaz <francedermaz@gmail.com>
2025-04-05 20:53:23 -0300

    chore: migration

---
+++ prisma/migrations/20250405235302_remove_reputation_field/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `reputation` on the `User` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "User" DROP COLUMN "reputation";
