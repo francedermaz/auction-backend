6b14e41588e11b73bf23c5d98d5481532a99a933
Francisco Cedermaz <francedermaz@gmail.com>
2025-04-05 20:52:49 -0300

    chore: empower humans to the future of internet

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -49,51 +49,6 @@ export const getUserByWallet = async (
   }
 };
 
-// PATCH /api/users/:wallet/reputation
-export const updateReputation = async (
-  req: Request,
-  res: Response
-): Promise<void> => {
-  const wallet = req.params.wallet;
-  const { reputation } = req.body;
-
-  try {
-    const user = await prisma.user.update({
-      where: { wallet },
-      data: { reputation },
-    });
-
-    res.json(user);
-  } catch (error) {
-    console.error("Error updating reputation:", error);
-    res.status(500).json({ error: "Internal server error" });
-  }
-};
-
-// GET /api/users/:wallet/reputation
-export const getReputation = async (
-  req: Request,
-  res: Response
-): Promise<void> => {
-  try {
-    const wallet = req.params.wallet;
-
-    const user = await prisma.user.findUnique({
-      where: { wallet },
-      select: { reputation: true },
-    });
-
-    if (!user) {
-      res.status(404).json({ error: "User not found" });
-      return;
-    }
-
-    res.json({ reputation: user.reputation });
-  } catch (error) {
-    res.status(500).json({ error: "Internal error" });
-  }
-};
-
 // GET /api/users/:wallet/history
 export const getUserHistory = async (
   req: Request,
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,18 +1,14 @@
 import { Router } from "express";
 import {
   createUser,
-  getReputation,
   getUserByWallet,
   getUserHistory,
-  updateReputation,
 } from "../controllers/userController";
 
 const router = Router();
 
 router.post("/", createUser);
 router.get("/:wallet", getUserByWallet);
-router.patch("/:wallet/reputation", updateReputation);
-router.get("/users/:wallet/reputation", getReputation);
 router.get("/users/:wallet/history", getUserHistory);
 
 export default router;
